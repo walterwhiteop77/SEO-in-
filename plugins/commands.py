85e46820cde8216e159c283fdc59013e25b4360a
walterwhiteop77 <walterwhite2k27@gmail.com>
2025-08-21 13:07:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -110,6 +110,8 @@ async def start(client, message):
                 ],[
                    InlineKeyboardButton('✦ ᴛʀᴇɴᴅɪɴɢ ✦', callback_data="topsearch"),
                     InlineKeyboardButton('✦ ᴘʀᴇᴍɪᴜᴍ ✦', callback_data="premium_info")
+                ],[
+                    InlineKeyboardButton('「 ✦ ᴏᴡɴᴇʀ ✦ 」', url='http://t.me/FilmyflixHD_Supportbot')
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
