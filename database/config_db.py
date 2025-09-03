aba8fc9e705b7a2bd3934a9561468eda05ae4dad
walterwhiteop77 <walterwhite2k27@gmail.com>
2025-09-03 20:37:24 +0530

    Update config_db.py

--- database/config_db.py
+++ database/config_db.py
@@ -10,6 +10,7 @@ class Database:
         self.col = self.db.user
         self.config_col = self.db.configuration
 
+    # ----------------- Top Messages Feature -----------------
     async def update_top_messages(self, user_id, message_text):
         user = await self.col.find_one(
             {"user_id": user_id, "messages.text": message_text}
@@ -35,6 +36,87 @@ class Database:
             {"$limit": limit},
         ]
         results = await self.col.aggregate(pipeline).to_list(limit)
+        return [result["_id"] for result in results]
+
+    async def delete_all_messages(self):
+        await self.col.delete_many({})
+
+    # ----------------- Advertisement Feature -----------------
+    def create_configuration_data(self, advertisement=None):
+        return {
+            "advertisement": advertisement,
+        }
+
+    async def update_advertisement(
+        self, ads_string=None, ads_name=None, expiry=None, impression=None
+    ):
+        config = await self.config_col.find_one({})
+        if not config:
+            await self.config_col.insert_one(self.create_configuration_data())
+            config = await self.config_col.find_one({})
+
+        advertisement = config.get("advertisement")
+
+        if advertisement is None:
+            advertisement = {}
+            config["advertisement"] = advertisement
+
+        advertisement["ads_string"] = ads_string
+        advertisement["ads_name"] = ads_name
+        advertisement["expiry"] = expiry
+        advertisement["impression_count"] = impression
+
+        await self.config_col.update_one(
+            {}, {"$set": {"advertisement": advertisement}}, upsert=True
+        )
+
+    async def update_advertisement_impression(self, impression=None):
+        await self.config_col.update_one(
+            {}, {"$set": {"advertisement.impression_count": impression}}, upsert=True
+        )
+
+    async def get_advertisement(self):
+        configuration = await self.config_col.find_one({})
+        if not configuration:
+            await self.config_col.insert_one(self.create_configuration_data())
+            configuration = await self.config_col.find_one({})
+        advertisement = configuration.get("advertisement", False)
+        if advertisement:
+            return (
+                advertisement.get("ads_string"),
+                advertisement.get("ads_name"),
+                advertisement.get("impression_count"),
+            )
+        return None, None, None
+
+    async def reset_advertisement_if_expired(self):
+        configuration = await self.config_col.find_one({})
+        if configuration:
+            advertisement = configuration.get("advertisement", False)
+            if advertisement:
+                impression_count = advertisement.get("impression_count", 0)
+                expiry = advertisement.get("expiry", None)
+                if (impression_count == 0) or (expiry and datetime.now() > expiry):
+                    await self.config_col.update_one(
+                        {}, {"$set": {"advertisement": None}}
+                    )
+
+    # ----------------- Generic Config -----------------
+    async def update_configuration(self, key, value):
+        try:
+            await self.config_col.update_one({}, {"$set": {key: value}}, upsert=True)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    async def get_configuration_value(self, key):
+        configuration = await self.config_col.find_one({})
+        if not configuration:
+            await self.config_col.insert_one(self.create_configuration_data())
+            configuration = await self.config_col.find_one({})
+        return configuration.get(key, False)
+
+
+mdb = Database(DATABASE_URI, "admin_database")        results = await self.col.aggregate(pipeline).to_list(limit)
         return [result["_id"] for result in results]
 
     async def delete_all_messages(self):
